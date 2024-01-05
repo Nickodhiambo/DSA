a8728317faa6e222642ffa1011ee4c612a80e2df
Nick <nodhiambo01@gmail.com>
2024-01-05 16:50:06 +0300

    two sum

---
+++ C-Arrays/single.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare and initialize an int array
+    int numArray[] = {0, 1, 2, 3, 4, 5};
+
+    // Access an element in array
+    printf("%d\n", numArray[4]);
+
+    // Declare and initialize a char array
+    char charArray[] = "hello";
+    printf("%c\n", charArray[4]);
+
+    return 0;
+}
