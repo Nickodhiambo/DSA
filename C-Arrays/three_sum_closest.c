6281b6f8662a006149e120ba52f48eb57bf03573
Nick <nodhiambo01@gmail.com>
2024-01-08 15:38:57 +0300

    Three sum closest

---
+++ C-Arrays/three_sum_closest.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+int threeSumClosest(int* nums, int numsSize, int target) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    // Initialize the closest sum variable
+    int closestSum = nums[0] + nums[1] + nums[2];
+
+    // Iterate through the array to find closest sum
+    for (int i = 0; i < numsSize - 2; i++)
+    {
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while (left < right)
+        {
+            int currentSum = nums[i] + nums[left] + nums[right];
+
+            // Update the closestSum if currentSum is closer to the target
+            if (abs(target - currentSum) < abs(target - closestSum))
+            {
+                closestSum = currentSum;
+            }
+            if (currentSum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    }
+    return (closestSum);
+}
